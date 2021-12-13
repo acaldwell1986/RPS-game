869320c15fbea8d520d55adb45c631e3404883a8
acaldwell1986 <acaldwell1986@gmail.com>
2021-12-13 10:57:01 -0500

    test

--- rps.js
+++ rps.js
@@ -1,125 +1,70 @@
 const buttons = document.querySelectorAll("button");
-const playerScore = document.querySelector(".playerScore");
-const compScore = document.querySelector(".compScore");
+const playerScoreDisplay = document.querySelector(".playerScore");
+const compScoreDisplay = document.querySelector(".compScore");
 const output = document.querySelector(".output")
 
 
-
-
-
-
-
-
-
+let playerChoice = '';
+let compChoice = '';
+let playerScore = 0;
+let computerScore = 0;
 
 buttons.forEach(button => { button.addEventListener('click', getPlayerChoice) });
 
-
-
 function getPlayerChoice(e) {
-    console.log(e.target.textContent);
-  }
+    playerChoice = e.target.textContent; 
+    playerChoice = playerChoice.toLowerCase();
+    console.log(gameRound(playerChoice, computerPlay()));
+}
+
+
+function computerPlay() {
+    const choice = Math.floor(Math.random()*100)
+        if (choice >= 67) {
+            return 'scissors'
+        }
+        else if (choice >= 33 && choice <= 66) {
+            return 'rock'
+        }
+        else
+            return 'paper'
+}
 
 
 
 
-
-
-
-
-// const rockBtn = document.querySelector('.rock');
-// const paperBtn = document.querySelector('.paper');
-// const scissorsBtn = document.querySelector('.scissors');
-
-// let playerScore = 0;
-// let computerScore = 0;
-// let tieCounter = 0;
-// let roundCount = 0;
-
-// // Randomly returns a value of Rock, Paper, or Scissors for CPU
-// function computerPlay() {
-//     const choice = Math.floor(Math.random()*100)
-//         if (choice >= 67) {
-//             return 'scissors'
-//         }
-//         else if (choice >= 33 && choice <= 66) {
-//             return 'rock'
-//         }
-//         else
-//             return 'paper'
-// }
-
-// function gameRound(playerSelection, computerSelection) { //returns the win/lose/tie message
-//     const play = playerSelection
-//     const comp = computerSelection
+function gameRound(playerSelection, computerSelection) { //returns the win/lose/tie message
+    const play = playerSelection
+    const comp = computerSelection
     
-//     //Tie game
-//     if (play === comp) {
-//         return 'Tie game!'
-//     }
+    //Tie game
+    if (play === comp) {
+        return 'Tie game!'
+    }
 
-//     else if (play === 'rock' && comp === 'paper') {
-//         return 'You lose! Rock covered by paper!'
-//     } 
-//     else if (play === 'rock' && comp === 'scissors') {
-//         return 'You win! Rock breaks scissors!'
-//     } 
+    else if (play === 'rock' && comp === 'paper') {
+        return 'You lose! Rock covered by paper!'
+    } 
+    else if (play === 'rock' && comp === 'scissors') {
+        return 'You win! Rock breaks scissors!'
+    } 
 
-//     else if (play === 'paper' && comp === 'scissors') {
-//         return 'You lose! Paper cut by scissors!'
-//     } 
-//     else if (play === 'paper' && comp === 'rock') {
-//         return 'You win! Paper covers rock!'
-//     } 
+    else if (play === 'paper' && comp === 'scissors') {
+        return 'You lose! Paper cut by scissors!'
+    } 
+    else if (play === 'paper' && comp === 'rock') {
+        return 'You win! Paper covers rock!'
+    } 
 
-//     else if (play === 'scissors' && comp === 'paper') {
-//         return 'You win! Scissors cut the paper!'
-//     } 
-//     else if (play === 'scissors' && comp === 'rock') {
-//         return 'You lose! Scissors crushed by rock!'
-//     } 
-// }
-
-// function gameScore (playerSelection, computerSelection) {
-//     const play = playerSelection
-//     const comp = computerSelection
+    else if (play === 'scissors' && comp === 'paper') {
+        return 'You win! Scissors cut the paper!'
+    } 
+    else if (play === 'scissors' && comp === 'rock') {
+        return 'You lose! Scissors crushed by rock!'
+    } 
+}
 
 
-//     if (play === comp) {
-//         tieCounter++;
-//         roundCount++;
-//     }
-//     else if (play === 'rock' && comp === 'paper') {
-//         computerScore++;
-//         roundCount++;
-//     } 
-//     else if (play === 'rock' && comp === 'scissors') {
-//         playerScore++;
-//         roundCount++;
-//     } 
-//     else if (play === 'paper' && comp === 'scissors') {
-//         computerScore++;
-//         roundCount++;
-//     } 
-//     else if (play === 'paper' && comp === 'rock') {
-//         playerScore++;
-//         roundCount++;
-//     } 
-//     else if (play === 'scissors' && comp === 'paper') {
-//         playerScore++;
-//         roundCount++;
-//     } 
-//     else if (play === 'scissors' && comp === 'rock') {
-//         computerScore++;
-//         roundCount++;
-//     } 
-
-//     console.log("Computer Score: " + computerScore);
-//     console.log("Round Count: " + roundCount);
-//     console.log("Player Score: " + playerScore);
-//     console.log("Tie Count: " + tieCounter);
-
-// }
 
 // function game() {
 
